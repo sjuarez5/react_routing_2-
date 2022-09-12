8ddd580e911c444de4b50d7924d4410ce7d8df77
Samuel Juarez-Solis <sjuarez-solis@catalyte.io>
2022-09-12 11:24:47 -0700

    fix QuoteDetail.js by importing Commnents as a default instead of a named export

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
-import { Comments } from "../components/comments/Comments";
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
